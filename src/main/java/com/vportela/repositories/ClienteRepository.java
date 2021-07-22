4e840b9c3cf37be799fc62a64b87590cd599a89a
Victor Portela <portelavictor@hotmail.com>
2021-07-22 16:03:44 -0300

    Cliente, TipoCliete, telefones e enderecos

---
+++ src/main/java/com/vportela/repositories/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.vportela.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vportela.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+	
+}
