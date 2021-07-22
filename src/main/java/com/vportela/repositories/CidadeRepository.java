8336dd1e6b5f8606876cc2eb85d503f6eeee93c5
Victor Portela <portelavictor@hotmail.com>
2021-07-22 15:08:40 -0300

    Estado e cidade

---
+++ src/main/java/com/vportela/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.vportela.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vportela.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+}
