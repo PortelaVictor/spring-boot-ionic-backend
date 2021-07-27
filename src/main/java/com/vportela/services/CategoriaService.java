cd7dd39828c2f3b14926469d255d704e10a9bf44
Victor Portela <portelavictor@hotmail.com>
2021-07-27 14:40:45 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/vportela/services/CategoriaService.java
+++ src/main/java/com/vportela/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.vportela.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.vportela.domain.Categoria;
 import com.vportela.repositories.CategoriaRepository;
+import com.vportela.services.exceptions.DataIntegrityException;
 import com.vportela.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,4 +33,13 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			 throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+		
+	}
 }
