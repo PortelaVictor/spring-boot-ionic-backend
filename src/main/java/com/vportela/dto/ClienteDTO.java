4741641cc468c98f42f49cd80e5133b79ea451dc
Victor Portela <portelavictor@hotmail.com>
2021-08-02 15:02:23 -0300

    Validacao customizada: email não repetido na atualizacao de Cliente

--- src/main/java/com/vportela/dto/ClienteDTO.java
+++ src/main/java/com/vportela/dto/ClienteDTO.java
@@ -8,8 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.vportela.domain.Cliente;
+import com.vportela.services.validation.ClienteUpdate;
 
-
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
